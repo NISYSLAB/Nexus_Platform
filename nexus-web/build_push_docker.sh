099a6aad5436c3b51ccb4de1f9d13e1332652709
Annie Gu <ping.gu@emory.edu>
2021-08-09 20:48:23 -0400

    20210809-web: put service in the docker

--- nexus-web/build_push_docker.sh
+++ nexus-web/build_push_docker.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_NAME=$(basename -- "$0")
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${SCRIPT_DIR} && source ./.common_configurations.sh
+cd "${SCRIPT_DIR}" && source ./.common_configurations.sh
 
 #### Function Definitions
 function build_web_jar() {
-    export GOOGLE_APPLICATION_CREDENTIALS="${PWD}/ssl/physionet-challenge-12lead-ecg-d875b52d05f9.json"
-    rm -rf ${web_jar}
+    ##export GOOGLE_APPLICATION_CREDENTIALS="${PWD}/ssl/physionet-challenge-12lead-ecg-d875b52d05f9.json"
+    rm -rf "${web_jar}"
     mvn clean && mvn package
-    ls -alt ${web_jar}
+    ls -alt "${web_jar}"
 }
 
 function build_image() {
@@ -17,27 +17,27 @@ function build_image() {
    local image_tag=$2
    local docker_file=$3
    echo "Build ${image_name}:${image_tag} docker image ..."
-   docker build  -f ${docker_file} -t ${image_name}:${image_tag} . || { echo "${image_name}:${image_tag} docker image build failed" ; exit 1; }
-   docker images |grep ${image_name}
+   docker build  -f "${docker_file}" -t "${image_name}":"${image_tag}" . || { echo "${image_name}:${image_tag} docker image build failed" ; exit 1; }
+   docker images |grep "${image_name}"
 }
 
 function push_image() {
    local image_name=$1
    local image_tag=$2
    echo "Push ${image_name}:${image_tag}  to docker hub"
-   docker push ${image_name}:${image_tag} || { echo "Push ${image_name}:${image_tag} to Docker Hub failed" ; exit 1; }
+   docker push "${image_name}:${image_tag}" || { echo "Push ${image_name}:${image_tag} to Docker Hub failed" ; exit 1; }
 }
 
 function build_push_image() {
    local image_name=$1
    local image_tag=$2
    local docker_file=$3
-   build_image ${image_name} ${image_tag} ${docker_file}
-   push_image ${image_name} ${image_tag}
+   build_image "${image_name}" "${image_tag}" "${docker_file}"
+   push_image "${image_name}" "${image_tag}"
 }
 #### End of Function Definitions
 #### Starts
-build_web_jar
-build_push_image ${web_image_name} ${web_image_tag} ${web_dockerfile}
+time build_web_jar
+time build_push_image "${docker_image_name}" "${docker_image_tag}" "${dockerfile}"
 
 
