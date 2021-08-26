c4c2a8ada0ecb3719b7e05659b31f9aeb932acb6
Annie Gu <ping.gu@emory.edu>
2021-08-26 16:27:19 -0400

    20210818-customized-api: Add task_header / task tables sql scripts

---
+++ nexus-web/postgres/sqlscript/v.1.1-create_task_tables.sql
@@ -0,0 +1,39 @@
+-- table task_header
+CREATE TABLE task_header (
+   task_header_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   yaml_config TEXT DEFAULT '',
+   json_config TEXT DEFAULT '',
+   completed BOOLEAN NOT NULL DEFAULT FALSE,
+   process_status VARCHAR(100) DEFAULT '',
+   time_submitted TIMESTAMP WITH TIME ZONE,
+   time_completed TIMESTAMP WITH TIME ZONE,
+   start_millis bigint default 0,
+   end_millis bigint
+);
+
+CREATE TABLE task (
+    task_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    parent_task_id UUID,
+    task_header_id UUID REFERENCES task_header (task_header_id),
+    task_index SMALLINT,
+    cromwell_id VARCHAR(100),
+    request_id VARCHAR(100),
+    wf_wdl_file TEXT DEFAULT '',
+    wf_input_file TEXT DEFAULT '',
+    wf_option_file TEXT DEFAULT '',
+    wf_type VARCHAR(100),
+    process_status VARCHAR(100) DEFAULT '',
+    project VARCHAR(500),
+    email VARCHAR(500),
+    label VARCHAR(500),
+    running_hours_allowed SMALLINT DEFAULT 24,
+    preemptible_option VARCHAR(200),
+    completed BOOLEAN NOT NULL DEFAULT FALSE,
+    result_output VARCHAR(1000),
+    note TEXT,
+    time_submitted TIMESTAMP WITH TIME ZONE,
+    time_completed TIMESTAMP WITH TIME ZONE,
+    start_millis bigint default 0,
+    end_millis bigint
+  );
+
--- nexus-web/src/main/java/edu/emory/cloudypipelines/nexusweb/bean/TaskMetadata.java
+++ nexus-web/src/main/java/edu/emory/cloudypipelines/nexusweb/bean/TaskMetadata.java
@@ -1,2 +1,23 @@
-package edu.emory.cloudypipelines.nexusweb.bean;public class TaskMetadata {
+package edu.emory.cloudypipelines.nexusweb.bean;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TaskMetadata {
+    private String groupUid = "";
+    private String cromwellId = "";
+    private int taskIndex = 0;
+    private String taskName = "";
+    private String taskStatus = "";
+    private String taskOutput = "";
+    private String taskProject = "";
 }
