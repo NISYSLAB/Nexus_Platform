8edfb40377b4a4fda8464351ff09df7ec51bb412
Annie Gu <ping.gu@emory.edu>
2021-10-23 21:40:35 -0400

    20211023_realtime_process: add deploy script

--- nexus-scheduler/build_jar.sh
+++ nexus-scheduler/build_jar.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 
-JAR=./target/nexus-scheduler-0.0.1-SNAPSHOT.jar
-rm -rf ${JAR}
+SCRIPT_NAME=$(basename -- "$0")
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ./common_settings.sh
+#### Start
+rm -rf "${JAR}"
+rm -rf "${APP_JAR}"
 mvn clean
-mvn package
-ls -alt ${JAR}
+mvn package || exit 1
+ls -alt "${JAR}"
+cp "${JAR}" ./"${APP_JAR}"
+
+echo "${SCRIPT_NAME}: Build VERSION: ${APP_JAR}"
+ls -alt "${APP_JAR}"
 
 ## mvn spring-boot:run
 ## gradle bootRun
