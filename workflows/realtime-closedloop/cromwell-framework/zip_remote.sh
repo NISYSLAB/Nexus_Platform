97c32c121beae3c3536cd3874c0cbe81dd3e41f7
Annie Gu <ping.gu@emory.edu>
2022-05-02 15:30:13 -0400

    20220502_cromwell_framework

--- workflows/realtime-closedloop/cromwell-framework/zip_remote.sh
+++ workflows/realtime-closedloop/cromwell-framework/zip_remote.sh
@@ -7,4 +7,5 @@ REMOTE_DIR=/home/pgu6/app/cromwell
 REMOTE_ZIP=remote_cromwell-framework.zip
 
 cd ${REMOTE_DIR}
+rm -rf ${REMOTE_ZIP}
 zip -r ${REMOTE_ZIP} ./*.sh ./.ssl/.settings.conf ${REMOTE_DIR}/.config/local_backend_local_filesystems.conf
