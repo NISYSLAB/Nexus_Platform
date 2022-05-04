ea6afbbc8f6188a36829b024a7c623b34ad95bfd
Annie Gu <ping.gu@emory.edu>
2022-05-04 17:25:17 -0400

    20220502_cromwell_framework: able to run csv gen

--- workflows/realtime-closedloop/cromwell-framework/wdl/exec_one.sh
+++ workflows/realtime-closedloop/cromwell-framework/wdl/exec_one.sh
@@ -10,42 +10,58 @@ function print_info() {
 }
 
 function dicom2nifti() {
-  print_info "dicom2nifti started"
-  mkdir -p ${dicomDir} && mkdir -p ${niiDir} && cp ${dicomInput} ${dicomDir}/input.tar.gz
-  cd ${dicomDir} && tar -xzf input.tar.gz && rm -f input.tar.gz && cd -
-  print_info "Files in directory: $(pwd)"
-  ls
-  print_info "Files in dicom directory: ${dicomDir}"
-  ls ${dicomDir}/
-  print_info "Calling: time python dicom_pypreprocess.py --filepath ${dicomDir} --savepath ${niiDir}"
-  time python dicom_pypreprocess.py --filepath ${dicomDir} --savepath ${niiDir}
-  print_info "Files in nii directory: ${niiDir}"
-  ls ${niiDir}
-  print_info "dicom2nifti completed"
+    print_info "dicom2nifti() started"
+    mkdir -p ${dicomDir} && mkdir -p ${niiDir} && cp ${dicomInput} ${dicomDir}/input.tar.gz
+    cd ${dicomDir} && tar -xzf input.tar.gz && rm -f input.tar.gz && cd -
+    print_info "Files in directory: $(pwd)"
+    ls
+    print_info "Files in dicom directory: ${dicomDir}"
+    ls ${dicomDir}/
+    print_info "Calling: time python dicom_pypreprocess.py --filepath ${dicomDir} --savepath ${niiDir}"
+    time python dicom_pypreprocess.py --filepath ${dicomDir} --savepath ${niiDir}
+    rtn_code=$?
+    print_info "dicom2nifti() user coding returned code=${rtn_code}"
+    print_info "Files in nii directory: ${niiDir}"
+    ls ${niiDir}
+    print_info "dicom2nifti() completed"
 }
 
 function rtpreproc() {
-  print_info "Under construction"
+  print_info "rtpreproc() started"
+  print_info "rtpreproc(): Under construction"
+  print_info "rtpreproc() completed"
 }
 
 function csvgen() {
-  echo "Under construction" > ${csvOutput}
+    print_info "csvgen() started"
+    cd ${currDir}
+    mkdir -p ${currDir}/csv
+    print_info "Files in directory: $(pwd)"
+    ls
+    print_info "Calling: python output_randomcsv.py --savepath ${currDir}/csv --savename ${csvOutput}"
+    time python output_randomcsv.py --savepath ${currDir}/csv --savename ${csvOutput}
+    rtn_code=$?
+    print_info "csvgen() user coding returned code=${rtn_code}"
+    print_info "Files in directory: csv"
+    ls csv
+    cp csv/${csvOutput}  ./${csvOutput}
+    print_info "csvgen() completed"
 }
 
 ####
 
 #### Main starts
 ## ./exec_realtime_loop.sh ${dicomInput} ${csvOutput} > ${log} 2>&1
+dicomDir="dicom"
+niiDir="nii"
+
 msg="Calling: ${SCRIPT_NAME} $@"
 print_info "${msg}"
 
 currDir=$PWD
 print_info "workDir=$currDir"
 
-dicomDir="dicom"
-niiDir="nii"
 argCt=2
-
 if [[ "$#" -ne ${argCt} ]]; then
     print_info "Invalid command line arguments, expecting $argCt"
     exit 1
