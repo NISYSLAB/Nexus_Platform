4b084aec6e72c672643ac41f17dbba58aebbdd5f
Annie Gu <ping.gu@emory.edu>
2022-07-07 17:10:14 -0400

    20220630_IO_monitor: add file size check

--- nexus-scheduler/deploy/midpoint_server/common_settings_template.sh
+++ nexus-scheduler/deploy/midpoint_server/common_settings_template.sh
@@ -10,17 +10,20 @@ export event_on_file_change=true
 export event_on_file_delete=false
 export event_on_file_create=true
 export execution_script=/mnt/drive0/synergyfernsync/synergy_process/scp2_bmi.sh
-export execution_folder=/mnt/drive0/synergyfernsync/synergy_process
+export execution_folder=/tmp/synergy/executions
 
 ## By setting corePoolSize and maximumPoolSize the same, you create a fixed-size thread pool
 ## The corePoolSize is the minimum number of workers to keep alive without timing out
-export executor_core_pool_size=5
+export executor_core_pool_size=10
 ## The maxPoolSize defines the maximum number of threads that can ever be created.
 ## To clarify, maxPoolSize depends on queueCapacity in that ThreadPoolTaskExecutor
 ## will only create a new thread if the number of items in its queue exceeds queueCapacity.
-export executor_max_pool_size=10
-export executor_queue_capacity=500
+export executor_max_pool_size=50
+export executor_queue_capacity=5000
 ## The amount of time in miliseconds to wait between checks of the file system
 export executor_check_interval=1000
+
+##
+export check_file_stable=true
 
 
