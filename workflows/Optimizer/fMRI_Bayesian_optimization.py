7dab7aaeafd8088c4d7ed1d82f2fe5039d8330b4
Yusen <yusenz@gatech.edu>
2022-07-08 02:18:30 -0400

    Revert "A few changes to incorporate task code settings."
    This reverts commit 6aa605346f9f5cebf4d584442da6f90f06da6da6.

--- workflows/Optimizer/fMRI_Bayesian_optimization.py
+++ workflows/Optimizer/fMRI_Bayesian_optimization.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue May 06 09:44:40 2022
+
 @author: parisa sarikhani (parisasarikhani@gmail.com)
-This python script reads the history of sampled parameters of the fMRI task from a CSV file,
+
+This python script reads the history of sampled parameters of the fMRI task from a CSV file, 
 and uses Bayesian optimization to suggest the next set of parameters to be sampled
+
 The next set of parameters to be sampled at the next round of experiment will be added as
 a line of output in a csv file in the desired path with the desired filename when called.
+
 This can either create a new file if it is at the befginning of the experiemnt or write a new line to an existing file otherwise
 .
 According to Michael the filename should be like 013_myfile.csv - the filename ordering is better handled in the shell script framework
+
 Arguments:
     --savepath - path of the output
     --savename - output filename
@@ -60,148 +65,154 @@ obj_filename = args.objectivepath
 
 
 def BayesOpt(filename, obj_filename):
-    rows=[]  # optimizer is always called after the task run at least 1 trial
+    
     flag_end = 0 # flag to the end of experiment
     if os.path.exists(filename):
         with open(filename) as f:
+            rows=[]
             for i, line in enumerate(f):
                 rows.append(line.replace('\n','').split(','))
-    #        print('rows', rows)
-    #        print(len(rows))
+#        print('rows', rows)
+#        print(len(rows))
+        # Read objective value (output of RC_proc)
+        if os.path.exists(obj_filename):
+            obj = []
+            with open(obj_filename) as f:
+                for i, line in enumerate(f):
+                    obj.append(line.replace('\n','').split(',')[0])
+        
+        
+        rows[len(rows)-1][2] = float(obj[0])
+        if len(rows)< N_burn_in: # initial random samples
+            
+            # rows[len(rows)-1][2] = obj
+            
+            reward_values = [0.2, 0.5, 0.8, 1.0]
+            #task parameters
+            q1 = str(np.random.randint(0,10)/2)
+            q2 = str(reward_values[np.random.randint(0,4)])
+    
+            # new_row = ['2', 'e100.png', 'r1.png', 'e100_r1.png', q1, q2]
+            new_row = [q1, q2, '', flag_end]
+            rows.append(new_row)
+            with open(filename, 'w') as csvfile:
+                # creating a csv writer object
+                csvwriter = csv.writer(csvfile)
+                # writing the data rows
+                csvwriter.writerows(rows)
+        else: #Suggested samples from Bayesian optimization:
+            
+            if len(rows)>=29:
+                flag_end = 1 # flag to the end of experiment
+            
+            
+            # Define the discrete parameter space:
+            q1_values = np.arange(0, 5, 0.5)
+            q2_values = [0.2, 0.5, 0.8, 1.0]
+            qq1, qq2 = np.meshgrid(q1_values, q2_values)
+            l = qq1.shape[0] * qq2.shape[1]
+            points = []
+            for i in range(l):
+                points.append([qq1.reshape(l)[i], qq2.reshape(l)[i]])
+        
+            domain = DiscreteSearchSpace(tf.constant(points))
+            
+            # Build GPR model:
+            def build_model(data):
+                X = data.query_points
+                Y = data.observations
+                q1 = X[:, 0]
+                q2 = X[:, 1]
+                #        variance = tf.math.reduce_variance(data.observations)
+                variance = tf.math.reduce_variance(Y) / np.sqrt(2) + 1e-12
+                kernel = gpflow.kernels.Matern52(variance=variance,
+                                                 lengthscales=[tf.math.reduce_variance(q1), tf.math.reduce_variance(q2)])
+                prior_scale = tf.cast(1.0, dtype=tf.float64)
+                gpr = gpflow.models.GPR(data.astuple(), kernel, noise_variance=1e-4)
+                gpflow.set_trainable(gpr.likelihood, False)
+        
+                return GaussianProcessRegression(gpr, num_kernel_samples=100)
+            
+            # Returns the objective value for each set of input task parameters
+            '''
+            #TODO
+            We will use the function "objecttive_values" once we have the output 
+            of the RT_Proc module and we will read the RT_Proc output from a file
+            '''
+            def objecttive_values(x):
+                y = []
+                for i, query in enumerate(x):
+                    y.append(Y_all[i])
+                return np.array([y]).T
+            
+#            Y_all needs to be updated here
+            q1_history = np.array([np.array(rows)[:,0].tolist()])
+            q2_history = np.array([np.array(rows)[:,1].tolist()])
+            task_params = np.concatenate((q1_history, q2_history), axis=0).T
+            query_points = tf.constant(task_params, dtype=tf.float64)
+            Y_all = np.array([float(i) for i in np.array(rows)[:,2]])
+            
+            # The scaled_branin function is a placeholder for the real objective value
+            # observer = trieste.objectives.utils.mk_observer(scaled_branin)
+            # collected_data = observer(query_points)
+            
+            observer = trieste.objectives.utils.mk_observer(objecttive_values)
+            collected_data = observer(query_points)
+            print(collected_data)
+            logging.info(collected_data)
+    
+            model = build_model(collected_data)
+            #        gpflow_model = build_gpr(collected_data, domain, likelihood_variance=1e-5)
+            #        model = GaussianProcessRegression(gpflow_model, num_kernel_samples=100)
+    
+            bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, domain)
+            num_steps = 1
+            result, history = (bo.optimize(num_steps, collected_data, model).astuple())
+            if result.is_ok:
+                #            print('result------', result)
+                #            print('history-----', history)
+                dataset = result.unwrap().dataset
+                query_points = dataset.query_points
+                new_row = [query_points[-1][0].numpy(), query_points[-1][1].numpy(), '', flag_end]
+                rows.append(new_row)
+            else:
+    
+                print('====================================================')
+                #            print('Erorrrrrrrrrrr', history)
+                dataset = history[0].dataset
+                query = dataset.query_points.numpy().tolist()
+                query.append(query[-1])
+                new_row = [query[-1][0], query[-1][1], '', flag_end]
+                rows.append(new_row)
 
-    # Read objective value (output of RC_proc)
-    if os.path.exists(obj_filename):
-        obj = []
-        with open(obj_filename) as f:
-            for i, line in enumerate(f):
-                obj.append(line.replace('\n','').split(',')[0])
-
-    current_obj = obj[0]
-    # rows[len(rows)-1][2] = float(obj[current_obj])
-    if len(rows)< N_burn_in: # initial random samples
+            with open(filename, 'w') as csvfile:
+                # creating a csv writer object
+                csvwriter = csv.writer(csvfile)
+                # writing the data rows
+                csvwriter.writerows(rows)
+            
+            
+            
+    
+    
+    else: # the first round where there is no csv file
         reward_values = [0.2, 0.5, 0.8, 1.0]
         #task parameters
         q1 = str(np.random.randint(0,10)/2)
         q2 = str(reward_values[np.random.randint(0,4)])
-        write_output_row(rows,q1,q2,current_obj,flag_end)
-    else: #Suggested samples from Bayesian optimization:
+    
+        # data rows of csv file
+        rows = [[q1, q2, '', str(flag_end)]]
+        # writing to csv file
 
-        if len(rows)>=29:
-            flag_end = 1 # flag to the end of experiment
-
-
-        # Define the discrete parameter space:
-        q1_values = np.arange(0, 5, 0.5)
-        q2_values = [0.2, 0.5, 0.8, 1.0]
-        qq1, qq2 = np.meshgrid(q1_values, q2_values)
-        l = qq1.shape[0] * qq2.shape[1]
-        points = []
-        for i in range(l):
-            points.append([qq1.reshape(l)[i], qq2.reshape(l)[i]])
-
-        domain = DiscreteSearchSpace(tf.constant(points))
-
-        # Build GPR model:
-        def build_model(data):
-            X = data.query_points
-            Y = data.observations
-            q1 = X[:, 0]
-            q2 = X[:, 1]
-            #        variance = tf.math.reduce_variance(data.observations)
-            variance = tf.math.reduce_variance(Y) / np.sqrt(2) + 1e-12
-            kernel = gpflow.kernels.Matern52(variance=variance,
-                                             lengthscales=[tf.math.reduce_variance(q1), tf.math.reduce_variance(q2)])
-            prior_scale = tf.cast(1.0, dtype=tf.float64)
-            gpr = gpflow.models.GPR(data.astuple(), kernel, noise_variance=1e-4)
-            gpflow.set_trainable(gpr.likelihood, False)
-
-            return GaussianProcessRegression(gpr, num_kernel_samples=100)
-
-        # Returns the objective value for each set of input task parameters
-        '''
-        #TODO
-        We will use the function "objecttive_values" once we have the output 
-        of the RT_Proc module and we will read the RT_Proc output from a file
-        '''
-        def objecttive_values(x):
-            y = []
-            for i, query in enumerate(x):
-                y.append(Y_all[i])
-            return np.array([y]).T
-
-        #            Y_all needs to be updated here
-        q1_history = np.array([np.array(rows)[:,0].tolist()])
-        q2_history = np.array([np.array(rows)[:,1].tolist()])
-        task_params = np.concatenate((q1_history, q2_history), axis=0).T
-        query_points = tf.constant(task_params, dtype=tf.float64)
-        Y_all = np.array([float(i) for i in np.array(rows)[:,2]])
-        # the current objective is the result generated by the last set of parameters sent to task computer
-        Y_all = np.append(Y_all[1:], float(current_obj))
-
-        # The scaled_branin function is a placeholder for the real objective value
-        # observer = trieste.objectives.utils.mk_observer(scaled_branin)
-        # collected_data = observer(query_points)
-
-        observer = trieste.objectives.utils.mk_observer(objecttive_values)
-        collected_data = observer(query_points)
-        print(collected_data)
-        logging.info(collected_data)
-
-        model = build_model(collected_data)
-        #        gpflow_model = build_gpr(collected_data, domain, likelihood_variance=1e-5)
-        #        model = GaussianProcessRegression(gpflow_model, num_kernel_samples=100)
-
-        bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, domain)
-        num_steps = 1
-        result, history = (bo.optimize(num_steps, collected_data, model).astuple())
-        if result.is_ok:
-            #            print('result------', result)
-            #            print('history-----', history)
-            dataset = result.unwrap().dataset
-            query_points = dataset.query_points
-            write_output_row(rows,query_points[-1][0].numpy(), query_points[-1][1].numpy(),current_obj,flag_end)
-        else:
-
-            print('====================================================')
-            #            print('Erorrrrrrrrrrr', history)
-            dataset = history[0].dataset
-            query = dataset.query_points.numpy().tolist()
-            query.append(query[-1])
-            write_output_row(rows,query[-1][0], query[-1][1],current_obj,flag_end)
-
-
-
-
-    # optimizer is always called after the task run at least 1 trial, this will be handled in burnout easily
-    # else: # the first round where there is no csv file
-    #     reward_values = [0.2, 0.5, 0.8, 1.0]
-    #     #task parameters
-    #     q1 = str(np.random.randint(0,10)/2)
-    #     q2 = str(reward_values[np.random.randint(0,4)])
-    #
-    #     # data rows of csv file
-    #     rows = [[q1, q2, '', str(flag_end)]]
-    #     # writing to csv file
-    #
-    #     with open(filename, 'w') as csvfile:
-    #         # creating a csv writer object
-    #         csvwriter = csv.writer(csvfile)
-    #         # writing the data rows
-    #         csvwriter.writerows(rows)
-
-
-def write_output_row(rows,q1,q2,obj,flag_end):
-    new_row = [str(q1), str(q2), str(obj), str(flag_end)]
-    rows.append(new_row)
-
-    with open(filename, 'w') as csvfile:    # filename is global in this file, this is bad programming practice probably
-        # creating a csv writer object
-        csvwriter = csv.writer(csvfile, dialect='unix', quoting=csv.QUOTE_MINIMAL)  # specifying the format helps for testing without linux environment
-        # writing the data rows
-        csvwriter.writerows(rows)
-        print('Optimizer generated new output in line %d' % (len(rows)))
-
-
+        with open(filename, 'w') as csvfile:
+            # creating a csv writer object
+            csvwriter = csv.writer(csvfile)
+            # writing the data rows
+            csvwriter.writerows(rows)
+        
+        
+        
 def main():
     # main
     BayesOpt(filename, obj_filename)
