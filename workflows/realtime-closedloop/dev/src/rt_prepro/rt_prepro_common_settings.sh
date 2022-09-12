1b487350540591bc192dd7630c19558af3bbe26c
Annie Gu <ping.gu@emory.edu>
2022-09-12 12:00:24 -0400

    20220912-pipeline

--- workflows/realtime-closedloop/dev/src/rt_prepro/rt_prepro_common_settings.sh
+++ workflows/realtime-closedloop/dev/src/rt_prepro/rt_prepro_common_settings.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename -- "$0")
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ${SCRIPT_DIR}
+source ../../common_settings.sh
+echo "PROFILE=${PROFILE}"
 
 #### Do Not  modify below!!!
-PROFILE=DEV
-
 MATLAB_VER=R2021b
 MCRROOT=/usr/local/MATLAB/${MATLAB_VER}
 
