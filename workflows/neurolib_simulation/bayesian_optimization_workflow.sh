a727bac893593fffd3bb2834a3aaaf4484a2369a
Yusen Zhu <yusenz@gatech.edu>
2023-11-10 10:29:51 -0500

    Loop order change, I thought I fixed it

--- workflows/neurolib_simulation/bayesian_optimization_workflow.sh
+++ workflows/neurolib_simulation/bayesian_optimization_workflow.sh
@@ -91,8 +91,8 @@ function execMain() {
     # for subject_id in $(seq 1 ${NUM_NEWSUBJECT}); do
     #     generateSubject ${SESSION_NAME} ${subject_id}
     # done
-    for expr_name in ${acqusition}; do
-        for subject_id in $(seq 1 $((${NUM_NEWSUBJECT} * ${NUM_SUBJECTREPEATS}))); do
+    for subject_id in $(seq 1 $((${NUM_NEWSUBJECT} * ${NUM_SUBJECTREPEATS}))); do
+        for expr_name in ${acqusition}; do
             subjectFlow ${expr_name} ${subject_id}
         done
     done
